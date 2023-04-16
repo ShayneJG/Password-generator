c64689e821ad31e58a89f69a0aa1b98a1ae200a3
Shayne Geilman <shaynegeilman@gmail.com>
2023-04-17 06:06:37 +0800

    centered app

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,14 @@ import "./App.css";
 // - See hover and focus states for all interactive elements on the page
 
 function App() {
-  return <div className="fixed top-0 left-0 right-0 bottom-0 m-auto">test</div>;
+  return (
+    <div
+      className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2
+"
+    >
+      <div>test</div>
+    </div>
+  );
 }
 
 export default App;
