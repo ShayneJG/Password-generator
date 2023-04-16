cbab24ca60f04cbfa46a6cf55d44ebb4cf3425d6
Shayne Geilman <shaynegeilman@gmail.com>
2023-04-16 17:17:09 +0800

    assets and positioning added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
 import "./App.css";
+// user stories
+// - Generate a password based on the selected inclusion options
+// - Copy the generated password to the computer's clipboard
+// - See a strength rating for their generated password
+// - View the optimal layout for the interface depending on their device's screen size
+// - See hover and focus states for all interactive elements on the page
 
 function App() {
-  return <div></div>;
+  return <div className="fixed top-0 left-0 right-0 bottom-0 m-auto">test</div>;
 }
 
 export default App;
