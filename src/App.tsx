c352298dfa33d98179df54784f6ce9d248894fc6
Shayne Geilman <shaynegeilman@gmail.com>
2023-04-17 14:38:44 +0800

    slider added

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,30 @@ import "./App.css";
 
 function App() {
   const [password, setPassword] = useState("");
+  const [length, setLength] = useState(10);
   return (
     <div
-      className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2
+      className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-almostWhite
 "
     >
       <div id="container" className="w-[375px] h-[667px] bg-[#08070B]">
         <h1>Password Generator</h1>
         <div id="password">{password}</div>
-        <div id="option-container"></div>
+        <div id="option-container">
+          <div id="slider-container">
+            <p>Character Length</p>
+            <p>{length}</p>
+            <input
+              type="range"
+              value={length}
+              min={1}
+              max={20}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setLength(e.target.valueAsNumber);
+              }}
+            />
+          </div>
+        </div>
         <div id="strength"></div>
         <div id="generate-button"></div>
       </div>
