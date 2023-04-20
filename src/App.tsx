f73c44efec61b7744d879a9955f3bcf86f576f4a
Shayne Geilman <shaynegeilman@gmail.com>
2023-04-20 08:44:59 +0800

    password function completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 
 import "./App.css";
 // user stories
@@ -23,12 +23,7 @@ function App() {
     >
       <div id="container" className="">
         <h1>Password Generator</h1>
-        <input
-          readOnly
-          id="password"
-          value={password}
-          placeholder="P4$5W0rD!"
-        />
+        <input id="password" value={password} placeholder="P4$5W0rD!" />
         <div className="bg-darkGrey p-5">
           <div id="option-container" className="">
             <div id="slider-container">
@@ -96,6 +91,9 @@ function App() {
           <div
             id="generate-button"
             className="flex items-center bg-neonGreen text-darkGrey justify-center w-full h-16 box-content hover:cursor-pointer"
+            onClick={() => {
+              generatePassword(length, upper, lower, num, sym, setPassword);
+            }}
           >
             <p className="pr-5">Generate </p>
             <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg">
@@ -120,10 +118,70 @@ function generatePassword(
   upper: boolean,
   lower: boolean,
   num: boolean,
-  sym: boolean
+  sym: boolean,
+  setPassword: React.Dispatch<SetStateAction<string>>
 ) {
   const upperLetters: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const lowerLetters: string = "abcdefghijklmnopqrstuvwxyz";
   const numbers: string = "0123456789";
   const symbols: string = `!@#$%^&*()-_=+[{]}\\|;:\'",.<>/?`;
+
+  let result: string[] = [];
+
+  let allChar: string = "";
+
+  //ensures at least one of each if they are meant to be included.
+  if (upper == true) {
+    result.push(upperLetters.charAt(getRandomNumber(upperLetters.length)));
+    allChar += upperLetters;
+  }
+  if (lower == true) {
+    result.push(lowerLetters.charAt(getRandomNumber(lowerLetters.length)));
+    allChar += lowerLetters;
+  }
+  if (sym == true) {
+    result.push(symbols.charAt(getRandomNumber(symbols.length)));
+    allChar += symbols;
+  }
+  if (num == true) {
+    result.push(numbers.charAt(getRandomNumber(numbers.length)));
+    allChar += numbers;
+  }
+
+  //generates the rest randomly
+  for (let i: number = 3; i < length - 1; i++) {
+    result.push(allChar.charAt(getRandomNumber(allChar.length)));
+  }
+
+  //shuffles the array to generate random order of characters
+  shuffle(result);
+
+  //sets the state to update the password
+  setPassword(result.join(""));
+}
+
+//gets random number between 0 and n.
+function getRandomNumber(n: number): number {
+  return Math.floor(Math.random() * (n + 1));
+}
+
+//
+function shuffle(array: string[]) {
+  let currentIndex: number = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
 }
